8cd24f5078e21750d370ecd90ce7a02465ecc0cc
The Geek on Skates <geekonskates@protonmail.com>
2023-02-20 16:22:50 -0500

    Added "git-branch"

--- git.fs
+++ git.fs
@@ -6,5 +6,6 @@
 : git-add-all s" git add --all" system ;
 : git-commit s" git commit" system ;
 : git-status s" clear && git status" system ;
+: git-branch s" git branch -a" system ;
 : git-log s" clear && git log --oneline --all --graph --decorate" system ;
 
